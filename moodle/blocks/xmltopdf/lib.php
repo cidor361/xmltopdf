e56466605fdf8ba1633503886a7edd7be12190f9
Igor Grebennikov <cidor361@gmail.com>
2019-02-13 11:21:24 +0300

    Not working!!!  * add simple html constructor

--- moodle/blocks/xmltopdf/lib.php
+++ moodle/blocks/xmltopdf/lib.php
@@ -2,3 +2,14 @@
 function XMLParsing($xmlCVObject) {
 
 }
+
+function TagB($string) {
+    $string2 = '<b>'.$string.'</b>';
+     return $string2;
+}
+
+
+
+function get_First_name($string) {
+    
+}
