044b9c5e826aa8e50f429bbb69724cd8d604657a
papergallery <trufanov@phys.vsu.ru>
2019-02-14 11:46:52 +0300

    Add fun

--- moodle/blocks/xmltopdf/lib.php
+++ moodle/blocks/xmltopdf/lib.php
@@ -24,12 +24,35 @@ function get_web_sites($XMLObject) {
 }
 function get_messagers($XMLObject){
 
-    $contacts = $XMLObject->LearnerInfo->Identification->ContactInfo->InstantMessagingList->InstantMessaging->{'0'}->Use->Code;
-    return $contacts;
+    $skype = $XMLObject->LearnerInfo->Identification->ContactInfo->InstantMessagingList->InstantMessaging->{'0'}->Use->Code;
+    return $skype;
+    $icq = $XMLObject->LearnerInfo->Identification->ContactInfo->InstantMessagingList->InstantMessaging->{'1'}->Use->Code;
+    return $icq;
+    $aim = $XMLObject->LearnerInfo->Identification->ContactInfo->InstantMessagingList->InstantMessaging->{'2'}->Use->Code;
+    return $aim;
+    $msn = $XMLObject->LearnerInfo->Identification->ContactInfo->InstantMessagingList->InstantMessaging->{'3'}->Use->Code;
+    return $msn;
+    $yahoo = $XMLObject->LearnerInfo->Identification->ContactInfo->InstantMessagingList->InstantMessaging->{'4'}->Use->Code;
+    return $yahoo;
 }
-function get_personal_info($XMLObject) {}
-function get_work_experiance($XMLObject) {}
-function get_education($XMLObject) {}
-function get_skills($XMLObject) {}
-function get_achivment($XMLObject) {}
+function get_personal_info($XMLObject) {
+    $statement = $XMLObject->LearnerInfo->Headline->Type->Code->Label;
+    return $statement;
+}
+function get_work_experiance($XMLObject) {
+    $work = $XMLObject->LearnerInfo->WorkExperienceList;
+    return $work;
+}
+function get_education($XMLObject) {
+    $education = $XMLObject->LearnerInfo->Skills;
+    return $education;
+}
+function get_skills($XMLObject) {
+    $skills = $XMLObject->LearnerInfo->EducationList;
+    return $skills;
+}
+function get_achivment($XMLObject) {
+    $achivment = $XMLObject->LearnerInfo->AchievementList;
+    return $achivment;
+}
 function get_cover_letter($XMLObject) {}
