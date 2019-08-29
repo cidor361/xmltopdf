d4d9db22f4db7f233263bfae9d2af19d69e1b914
Igor Grebennikov <cidor361@gmail.com>
2019-08-29 10:06:07 +0300

    Coursefields:     * added couple functions for interacting with courses (Fixed bug)

--- moodle/blocks/coursefields/constructor.php
+++ moodle/blocks/coursefields/constructor.php
@@ -281,7 +281,7 @@ function get_status_of_course($url, $course_id_ext, $info) {
     $resp = curl_exec($curl);
     $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
     if ($status != 201) {
-        die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+        die("Error: call to URL $url failed with status $status, response $resp, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
     }
     curl_close($curl);
 }
