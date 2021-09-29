c0fddbea9fc5eeb926060a8d5081d4bf2340508d
Igor Grebennikov <cidor361@gmail.com>
2021-09-29 09:55:19 +0300

    Usefull code:     * added geting faculty and cafedra

--- Usefull code/statistic/2nd table.php
+++ Usefull code/statistic/2nd table.php
@@ -3,7 +3,12 @@
 //Информация по курсам
 $query_course_info = "SELECT id AS courseid, shortname, fullname FROM mdl_course ;";
 
-/* Факультет и Кафедра - категории курсов
+// Факультет и Кафедра - категории курсов
+$cafedra_info = $DB->get_record('course_categories', array('id' => $course->category), 'name,parent');
+$cafedra = $cafedra_info->name;
+$parent = $cafedra_info->parent;
+$faculty = $DB->get_record('course_categories', array('id' => $parent), 'name');
+$faculty = $faculty->name;
 
 //TODO: Узнать про код дисциплин семестр и форму обучения
 
