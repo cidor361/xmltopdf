f713dfa1dc75fbca57c95e4aa602d5fe435a9db0
Igor Grebennikov <grebennikov@phys.vsu.ru>
2021-10-04 19:40:33 +0300

    Usermanager:     * change version

--- moodle/blocks/usermanager/version.php
+++ moodle/blocks/usermanager/version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_usermanager';
-$plugin->version = 2021092300;
+$plugin->version = 2021100400;
 $plugin->requires = 2011062400;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = array(
